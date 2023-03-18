b3af74aa9c4ad2fcd6a57fc4349adf1f896c82d6
Seah Yok Sim <yoksim.seah.2021@scis.smu.edu.sg>
2023-03-18 15:07:23 +0800

    Resolve errors in student.grqphql

--- Student Microservice/student.py
+++ Student Microservice/student.py
@@ -1,66 +1,3 @@
-# import os
-# from flask import Flask, request, jsonify
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_cors import CORS
-
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/book'
-# # dialect+driver://username:password@host:port/database
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-
-# db = SQLAlchemy(app)
-# CORS(app)
-
-# class Student(db.Model):
-#     __tablename__ = 'student'
-
-
-#     student_id = db.Column(db.Integer, primary_key=True, nullable=False)
-#     student_name = db.Column(db.String(50), nullable=False)
-#     email = db.Column(db.String(50), nullable=False)
-#     is_subscribed = db.Column(db.SmallInteger, nullable=False)
-#     is_graduated = db.Column(db.SmallInteger, nullable=False)
-
-
-#     def __init__(self, student_id, student_name, email, is_subscribed, is_graduated):
-#         self.student_id = student_id
-#         self.student_name = student_name
-#         self.email = email
-#         self.is_subscribed = is_subscribed
-#         self.is_graduated = is_graduated
-
-
-#     def json(self):
-#         return {"student_id": self.student_id, "name": self.student_name, "email": self.email, "grad": self.is_graduated, "sub": self.is_subscribed}
-
-# @app.route("/students/<string:student_id>/modules")
-# def list_modules_by_id(student_id):
-#     student = Student.query.filter_by(student_id=student_id).first()
-#     if student:
-#         return jsonify(
-#             {
-#                 "code": 200,
-#                 "data": student.json()
-#             }
-#         )
-#     return jsonify(
-#         {
-#             "code": 404,
-#             "data": {
-#                 "student_id": student_id
-#             },
-#             "message": "student not found."
-#         }
-#     ), 404
-
-# @app.route("/")
-# def home():
-#     return "hello"
-
-# if __name__ == '__main__':
-#     app.run(port=5200, debug=True)
-
 from ariadne import load_schema_from_path, make_executable_schema, graphql_sync, snake_case_fallback_resolvers, \
     ObjectType
 from flask import Flask, request, jsonify
@@ -70,7 +7,8 @@ from student_models import db
 from student_queries import *
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/module'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/module'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/student'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.app_context().push()
 db.init_app(app)
@@ -107,3 +45,6 @@ def graphql_server():
     )
     status_code = 200 if success else 400
     return jsonify(result), status_code
+
+if __name__ == '__main__':
+    app.run(port=5200, debug=True)
